1d17b7cd17d87877f761d4b7db5fc8e0a8e4d107
Murtada Altarouti <murtada.altarouti@gmail.com>
2025-03-01 23:11:05 +0300

    Added Q1684 Solutions

--- leetcode-1684/solution-2.py
+++ leetcode-1684/solution-2.py
@@ -5,12 +5,9 @@ class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         count = 0
         for word in words:
-            flag = True
             for letter in word:
                 if letter not in allowed:
-                    flag = False
-
-            if flag is True:
+                    break
+            else:
                 count += 1
-
         return count
--- leetcode-1684/solution-3.py
+++ leetcode-1684/solution-3.py
@@ -5,12 +5,6 @@ class Solution:
     def countConsistentStrings(self, allowed: str, words: List[str]) -> int:
         count = 0
         for word in words:
-            flag = True
-            for letter in word:
-                if letter not in allowed:
-                    flag = False
-
-            if flag is True:
+            if len(set(word) - set(allowed)) == 0:
                 count += 1
-
         return count
